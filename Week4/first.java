d2223f72b185860a9f00b40bb0f2ce30fc6a88c8
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-03-31 10:41:44 +0530

    Create first.java

---
+++ Week4/first.java
@@ -0,0 +1,16 @@
+//Take name rollno. & college using Command line arguments
+import java.util.*;
+
+public class first{
+	public static void main(String [] args){
+String name=args[0];
+String roll=args[1];
+String 
+	for(String i:args)
+{	
+System.out.println(i);
+}
+
+	}
+
+}
