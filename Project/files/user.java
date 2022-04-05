d6b7667a7d6dc144daddacb8cc9b076e3a739bb2
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-04-05 23:49:20 +0530

    Create user.java

---
+++ Project/files/user.java
@@ -0,0 +1,16 @@
+package files;
+import java.util.*;
+public class user{
+	 public void menuprint()
+	    {
+	        System.out.println("Operations for User menu");
+	        System.out.println("1. Search For Cars \n2. Show Loan Record of a user\n"
+	        		+ "3. Return to Main Menu");
+	    }
+	public void search(){
+        //search using three methods
+    }
+	public void show(){
+        //show renting record based on user id
+    }
+}
