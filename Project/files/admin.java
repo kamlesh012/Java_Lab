751a7315f83fea8637952f87d5e47f4857f28805
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-04-05 23:48:32 +0530

    Create admin.java

---
+++ Project/files/admin.java
@@ -0,0 +1,32 @@
+package files;
+import java.util.*;
+
+public class admin{
+	public void menuprint()
+    {
+        System.out.println("Operations for Administrator menu");
+        System.out.println("1. Create all tables\n2. Delete all\n3. Load from datafile\n4. Show number of records in each table\n5.Return to the main menu");
+    }
+
+	public void createTable()
+    {
+
+    }
+
+	public void deletetable()
+    {
+
+    }
+	public void LoadData()
+    {
+        //User will enter path of the folder that contains data files,
+
+    }
+    
+	public void Show_records()
+    {
+
+    }
+    
+
+  }
