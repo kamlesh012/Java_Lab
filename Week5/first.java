a2b3aa456cadb4377c46a0f399c56a41c9768671
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-04-07 09:44:07 +0530

    Create first.java

---
+++ Week5/first.java
@@ -0,0 +1,46 @@
+////WAP to arrange an array in zig zag pattern
+import java.util.*;
+public class first{
+	public static void main(String args[]){
+
+	Scanner inp=new Scanner(System.in);
+
+	int n;
+	System.out.println("ENTER SIZE OF ARRAY");
+	n=inp.nextInt();
+
+	int a[]=new int[n];
+	for(int i=0;i<n;i++){
+	a[i]=inp.nextInt();
+	}
+/*
+	System.out.println("Original Array is");
+	for(int i=0;i<n;i++){
+	System.out.print(a[i]+" ");
+	}
+
+*/
+	for(int i=0;i<n-1;i++){
+		if( i%2==0 && a[i]>a[i+1] ){
+	
+		int temp=a[i];
+		a[i]=a[i+1];
+		a[i+1]=temp;
+
+		}
+		else if(i%2==1 && a[i]<a[i+1]){
+		int temp=a[i];
+		a[i]=a[i+1];
+		a[i+1]=temp;
+		}
+	}
+
+	System.out.println("ZigZag Array is");
+	for(int i=0;i<n;i++){
+	System.out.print(a[i]+" ");
+}
+
+
+	
+	}
+}
